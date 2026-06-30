76b06c42089b9a04b44b4f10895a4c009d606a7f
Nada Alaa-Eldien Hamdallah <133704223+nadadew20@users.noreply.github.com>
2026-06-30 08:48:02 +0300

    Update runDocker.groovy

--- vars/runDocker.groovy
+++ vars/runDocker.groovy
@@ -1,6 +1,11 @@
 //vars/runDocker.groovy
 def call(String appName, String buildNumber, String hostPort = "5000") {
     sh """
+    # Stop the old container if it exists
+                docker stop ${APP_NAME}-main || true
+
+                # Remove the old container if it exists
+                docker rm ${APP_NAME}-main || true
         docker run -d \
         --name ${appName}-main-${buildNumber} \
         -p ${hostPort}:5000 \
